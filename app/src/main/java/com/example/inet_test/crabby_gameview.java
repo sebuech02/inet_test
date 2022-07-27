038c2b79f4fb3b3e24ce855e1a9dc63c3c723d7f
sebuech02 <sebastian.buechler@tutanota.de>
2022-07-27 14:15:50 +0200

    cancer movement

--- app/src/main/java/com/example/inet_test/crabby_gameview.java
+++ app/src/main/java/com/example/inet_test/crabby_gameview.java
@@ -17,6 +17,7 @@ public class crabby_gameview extends View{
     private int width, height;
     private Bitmap crab;
     private int crabx, craby;
+    private float crabspeed = 0;
     private boolean einmal = true;
     private float input;
     private SensorManager sensorManager;
@@ -34,6 +35,7 @@ public class crabby_gameview extends View{
             @Override
             public void onSensorChanged(SensorEvent event) {
                 input = event.values[1];
+                crabspeed = crabspeed + input/20;
             }
 
             @Override
@@ -51,13 +53,20 @@ public class crabby_gameview extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        crabx =(int) (crabx + input);
+        if (crabspeed>0){
+            crabspeed =(float) (crabspeed - 0.2);
+        }
+        if (crabspeed<0){
+            crabspeed =(float) (crabspeed + 0.2);
+        }
+        crabx =(int) (crabx + crabspeed);
         if (crabx>width-crab.getWidth()){
             crabx = width-crab.getWidth();
+            crabspeed = 0;
         }
         if (crabx<0){
             crabx = 0;
+            crabspeed = 0;
         }
         canvas.drawBitmap(crab, crabx, craby,null);
         //Hier findet das Spiel statt. Diese Funktion wird 66 mal pro sekunde aufgerufen
