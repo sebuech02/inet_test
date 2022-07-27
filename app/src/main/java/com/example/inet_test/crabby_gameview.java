4290b06d3dfec6d24aee22e5446869c176ba9d80
sebuech02 <sebastian.buechler@tutanota.de>
2022-07-27 22:22:04 +0200

    drunkmode + reibung bei movement

--- app/src/main/java/com/example/inet_test/crabby_gameview.java
+++ app/src/main/java/com/example/inet_test/crabby_gameview.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -20,6 +22,7 @@ public class crabby_gameview extends View{
     private int width, height;
     private Bitmap crab;
     private float crabspeed = 0;
+    private float störung;
     private Bitmap pineapple;
     private Bitmap coconut;
     private Bitmap bananas;
@@ -29,7 +32,12 @@ public class crabby_gameview extends View{
     private float input;
     private SensorManager sensorManager;
     private Sensor sensor;
-
+    private int framecounter;
+    private boolean drunkmode;
+    private boolean taumeln_rechts;
+    private int duration = 66;
+    private Paint score_paint = new Paint();
+    private Bitmap live[] = new Bitmap[2];
     private int score, lives, counter;
 
     private int pineappleX, pineappleY, pineappleSpeed=7;
@@ -53,7 +61,14 @@ public class crabby_gameview extends View{
         // Die Früchte und andere Objekte
         pineapple = BitmapFactory.decodeResource(getResources(), R.drawable.ananas1);
         pineapple = Bitmap.createScaledBitmap(pineapple, width/10, height/10, false);
-
+        score_paint.setColor(Color.GREEN);
+        score_paint.setTextSize(70);
+        score_paint.setTypeface(Typeface.DEFAULT_BOLD);
+        score_paint.setAntiAlias(true);
+        live[0] = BitmapFactory.decodeResource(getResources(), R.drawable.heart);
+        live[0] = Bitmap.createScaledBitmap(live[0], width/10, height/10, false);
+        live[1] = BitmapFactory.decodeResource(getResources(), R.drawable.heart_broken);
+        live[1] = Bitmap.createScaledBitmap(live[1], width/10, height/10, false);
 
         // Bewegungssteuerung
         sensorManager = (SensorManager) getContext().getSystemService(Context.SENSOR_SERVICE);
@@ -62,7 +77,6 @@ public class crabby_gameview extends View{
             @Override
             public void onSensorChanged(SensorEvent event) {
                 input = event.values[1];
-                crabspeed = crabspeed + input/30;
             }
 
             @Override
@@ -76,39 +90,70 @@ public class crabby_gameview extends View{
         crab = Bitmap.createScaledBitmap(crab, width/10, height/10, false);
         crabY =(int) (height-1.3*crab.getHeight());
         crabX = (width-crab.getWidth())/2;
+        crabspeed = 0;
+        lives = 3;
+        drunkmode = false;
+
 
         // Sounds und Musik
         burp = MediaPlayer.create(getContext(), R.raw.burp);
         crabrave = MediaPlayer.create(getContext(), R.raw.crabrave);
         crabrave.isLooping();
         crabrave.start();
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        height = displayMetrics.heightPixels;
+        width = displayMetrics.widthPixels;
         canvas.drawBitmap(background_img,0,0,null);
-
+        if (drunkmode){
+            if (framecounter>=duration){
+                if (taumeln_rechts) {
+                    störung =(float) Math.random()/5;
+                    framecounter = 0;
+                    duration =(int) Math.floor(Math.random()*33);
+                    taumeln_rechts = false;
+                } else {
+                    störung =(float) (Math.random()-1)/5;
+                    framecounter = 0;
+                    duration =(int) Math.floor(Math.random()*33);
+                    taumeln_rechts = true;
+                }
+            } else {
+                framecounter = framecounter + 1;
+            }
+        }else{
+            störung = 0;
+        }
+        crabspeed = crabspeed + input/30 + störung;
 
         // This needs change
         int mincX = 0;
         int maxcX = width-crab.getWidth();
 
         if (crabspeed>0){
-            crabspeed =(float) (crabspeed - 0.2);
+            crabspeed =(float) (crabspeed - 0.01);
         }
         if (crabspeed<0){
-            crabspeed =(float) (crabspeed + 0.2);
+            crabspeed =(float) (crabspeed + 0.01);
         }
+
+
+        crabX =(int) (crabX + crabspeed);
 
         if (crabX >width-crab.getWidth()){
             crabX = width-crab.getWidth();
+            crabspeed = 0;
         }
         if (crabX <0){
+            crabspeed = 0;
             crabX = 0;
         }
-        crabX =(int) (crabX + crabspeed);
 
         pineappleY = pineappleY + pineappleSpeed;
 
@@ -125,6 +170,17 @@ public class crabby_gameview extends View{
         canvas.drawBitmap(crab, crabX, crabY,null);
         canvas.drawBitmap(pineapple, pineappleX, pineappleY, null);
         //Hier findet das Spiel statt. Diese Funktion wird 66 mal pro sekunde aufgerufen
+        canvas.drawText("Score: "+score, 20,60,score_paint);
+
+        for (int i = 0; i<3; i++){
+            int x = width-(3-i)*crab.getWidth();
+            int y = 10;
+            if (i<lives){
+                canvas.drawBitmap(live[0],x,y,null);
+            } else {
+                canvas.drawBitmap(live[1],x,y,null);
+            }
+        }
     }
 
     public boolean hitchecker(int x,int y){
